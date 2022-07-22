a8fce99543d5c0cee942a3d1dd603cc0f145aded
Andy <njloveqd@foxmail.com>
2022-07-22 16:50:02 +0800

    start dev nextjs blog

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import 'styles/global.scss'
 export default function App({ Component, pageProps }) {
   return <>
     <Head>
-      <title>我的博客 - Frank</title>
+      <title>我的博客 - Andy</title>
       <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
     </Head>
     <Component {...pageProps} />
